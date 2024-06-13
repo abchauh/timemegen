5c695abd27ba3a6ac359f395f0bd2307d65dc5a7
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2024-06-13 22:02:40 +0100

    Build fix

--- src/stickerLoader.ts
+++ src/stickerLoader.ts
@@ -1,3 +1,3 @@
-const stickers = import.meta.glob('./stickers/*.{png,jpg,jpeg,svg,webp}', { eager: true });
+const stickers = import.meta.glob<{ default: string }>('./stickers/*.{png,jpg,jpeg,svg,webp}', { eager: true });
 
 export default Object.keys(stickers).map((key) => stickers[key].default);
